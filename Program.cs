990769651a8cc4aa261a0135e84ec6c4c6ebfbf5
vika <kosolapova5.2006@mail.ru>
2026-05-17 22:33:43 +0300

    Изменение файла Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,45 @@
-using LibraryWebSystem.Components;
+using LibraryWebSystem.Data;
+using LibraryWebSystem.Models;
+using LibraryWebSystem.Services;
+using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+// Подключение к БД (SQLite)
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//  Регистрация сервиса поиска (Dependency Injection)
+builder.Services.AddScoped<ISearchService, SearchService>();
+
+//  Настройка Blazor Server
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Инициализация БД и тестовые данные 
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var db = scope.ServiceProvider.GetRequiredService<LibraryContext>();
+    db.Database.EnsureCreated();
+
+    if (!db.Books.Any())
+    {
+        var b1 = new Book { Title = "Мастер и Маргарита", YearPublished = 1967, Price = 479, FileFormat = "PDF" };
+        var a1 = new Author { FirstName = "Михаил", LastName = "Булгаков" };
+        db.BookAuthors.Add(new BookAuthor { Book = b1, Author = a1 });
+        db.Books.Add(b1);
+
+        var b2 = new Book { Title = "Война и мир", YearPublished = 1869, Price = 399, FileFormat = "EPUB" };
+        var a2 = new Author { FirstName = "Лев", LastName = "Толстой" };
+        db.BookAuthors.Add(new BookAuthor { Book = b2, Author = a2 });
+        db.Books.Add(b2);
+
+        await db.SaveChangesAsync();
+    }
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
+// Маршрутизация Blazor
+app.MapRazorComponents<LibraryWebSystem.Components.App>()
     .AddInteractiveServerRenderMode();
-
-app.Run();
